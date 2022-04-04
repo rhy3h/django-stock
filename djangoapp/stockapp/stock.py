96960b432a9259f13c48c45091d95156c41f3487
rhy3h <rhy3htw@gmail.com>
2022-04-04 15:28:29 +0800

    Stock Chart

--- djangoapp/stockapp/stock.py
+++ djangoapp/stockapp/stock.py
@@ -8,15 +8,12 @@ from datetime import date, datetime, timedelta
 from stockapp import models
 from stockapp.crawler import esun
 
-from backtesting import Strategy
-from backtesting.lib import crossover
-from backtesting.test import SMA
-
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import ColumnDataSource, HoverTool, BoxAnnotation, RangeSlider, Legend
+from bokeh.models import ColumnDataSource, HoverTool, BoxAnnotation, RangeSlider, Legend, CrosshairTool
 from bokeh.layouts import layout, row, Spacer
-from bokeh.models.callbacks import CustomJS
+from bokeh.models.callbacks import CustomJS 
+from bokeh.models import BasicTickFormatter
 
 @login_required
 def base(request):
@@ -34,17 +31,6 @@ def base(request):
         return redirect(f'/stock/2330')
     else:
         return render(request, 'stock.html', locals())
-
-class SmaCross(Strategy):
-    def init(self):
-        close = self.data.Close
-        self.sma1 = self.I(SMA, close, 5)
-        self.sma2 = self.I(SMA, close, 10)
-    def next(self):
-        if crossover(self.sma1, self.sma2):
-            self.buy()
-        elif crossover(self.sma2, self.sma1):
-            self.sell()
 
 @login_required
 def index(request, code):
@@ -71,31 +57,11 @@ def index(request, code):
         Code = code
     )
     
-    if request.POST.get('search'):
-        begin_date = request.POST.get('begin-date')
-        begin_datatime = datetime.strptime(begin_date, "%Y-%m-%d")
-        end_date = request.POST.get('end-date')
-        end_datatime = datetime.strptime(end_date, "%Y-%m-%d")
-
-        if end_datatime.isoweekday() > 5:
-            end_date = (end_datatime + timedelta(days = (5 - end_datatime.isoweekday()))).strftime("%Y-%m-%d")
-        if begin_datatime.isoweekday() > 5:
-            begin_date = (begin_datatime + timedelta(days = (5 - begin_datatime.isoweekday()))).strftime("%Y-%m-%d")
-
-        if begin_date > end_date:
-            begin_date, end_date = end_date, begin_date
-        
-        begin_date_split = begin_date.split('-')
-        new_begin_date = f"{begin_date_split[0]}-{int(begin_date_split[1])}-{int(begin_date_split[2])}"
-        end_date_split = end_date.split('-')
-        new_end_date = f"{end_date_split[0]}-{int(end_date_split[1])}-{int(end_date_split[2])}"
-
-        broker_table = esun.crawler(code, new_begin_date, new_end_date)
-
     df = pd.read_csv(f'djangoapp/stockapp/files/historical-daily-candlesticks/{code}.csv')
     df = df[::-1].reset_index()
     df = df.drop(columns=['index'])
     df.columns  = ['date', 'open', 'high', 'low', 'close', 'volume']
+    df['volume'] = df['volume'].astype(int)
     df['date']  = pd.to_datetime(df['date'])
     df['MA5']   = df['close'].rolling(5).mean()
     df['MA10']  = df['close'].rolling(10).mean()
@@ -106,7 +72,7 @@ def index(request, code):
     inc = df.close > df.open
     dec = df.open > df.close
 
-    date_length = 60
+    date_length = 120
     x_end = len(df)
     x_start = x_end - date_length
     y_start = df['close'][-date_length:].min() * 0.95
@@ -132,18 +98,21 @@ def index(request, code):
         },
         mode = 'vline'
     )
+    
     p1 = figure(
         title = f'{full_code_name} K線圖',
         tools = [hover, 'save'],
         x_range = (x_start, x_end),
         y_range = (y_start, y_end),
         plot_width = plot_width,
-        toolbar_location = 'above'
+        toolbar_location = 'above',
+        y_axis_location = 'right',
+        min_border_right = 50,
     )
     p1.xaxis.major_label_overrides = {
         i: date.strftime('%Y-%m-%d') for i, date in enumerate(pd.to_datetime(df['date']))
     }
-
+    
     df_source = ColumnDataSource(df)
     inc_data = df[inc]
     dec_data = df[dec]
@@ -164,7 +133,7 @@ def index(request, code):
         'close',
         fill_color = '#eb2409',
         line_color = 'black',
-        source= inc_source
+        source = inc_source
     )
     vbar2 = p1.vbar(
         'index',
@@ -196,56 +165,88 @@ def index(request, code):
         ma_legend_items.append((ma_name, [ma_line]))
     legend = Legend(
         items = ma_legend_items,
-        location = (50, 50)
+        location = (50, 350)
     )
     p1.add_layout(legend)
-
+    
     p2 = figure(
         x_range = (0, x_end),
         y_range = (df['close'].min(), df['close'].max()),
         plot_width = plot_width,
         plot_height = 100,
-        toolbar_location = None
+        toolbar_location = None,
+        y_axis_location = 'right',
+        min_border_right = 50,
     )
     p2.xaxis.major_label_overrides = {
         i: date.strftime('%Y-%m-%d') for i, date in enumerate(pd.to_datetime(df["date"]))
     }
     p2.line(
-        x='index',
-        y='close',
-        line_width=2,
-        color="black",
-        alpha=0.8,
-        source=df_source
+        x = 'index',
+        y = 'close',
+        line_width = 2,
+        color = "black",
+        alpha = 0.8,
+        source = df_source
     )
-    p2.yaxis.major_label_text_color = None
-    p2.yaxis.major_tick_line_color= None
-    p2.yaxis.minor_tick_line_color= None
-    p2.grid.grid_line_color=None
+    # p2.yaxis.major_label_text_color = None
+    #p2.yaxis.major_tick_line_color = None
+    p2.yaxis.minor_tick_line_color = None
+    p2.grid.grid_line_color = None
 
     box = BoxAnnotation(
         fill_alpha = 0.5,
         line_alpha = 0.5,
         level = 'underlay',
         left = x_start,
-        right = x_end
+        right = x_end,
     )
     p2.add_layout(box)
 
+    hover3 = HoverTool(
+        tooltips=[
+            ('volume' , '@volume')
+        ],
+        formatters = {
+        },
+        mode = 'vline'
+    )
+    y_start = df['volume'][-date_length:].min() * 0.95
+    y_end = df['volume'][-date_length:].max() * 1.05
+    p3 = figure(
+        x_range = (x_start, x_end),
+        y_range = (y_start, y_end),
+        tools = [hover3],
+        plot_width = plot_width,
+        plot_height = 100,
+        toolbar_location = None,
+        y_axis_location = 'right',
+        min_border_right = 50,
+    )
+    p3.yaxis.minor_tick_line_color = None
+    p3.vbar('index', 0.5, 'volume', line_color = 'black', fill_color = '#eb2409', source = inc_source)
+    p3.vbar('index', 0.5, 'volume', line_color = 'black', fill_color = '#00995c', source = dec_source)
+    p3.yaxis.formatter = BasicTickFormatter(use_scientific=False)
+    p3.xaxis.major_label_overrides = {
+        i: date.strftime('%Y-%m-%d') for i, date in enumerate(pd.to_datetime(df["date"]))
+    }
+    
     range_slider = RangeSlider(
         title = None,
         start = 0,
         end = x_end,
         value = (x_start, x_end),
-        default_size = plot_width-30,
+        default_size = plot_width - 50,
         step = 1
     )
-    range_slider.js_link('value', p1.x_range, 'start', attr_selector=0)
-    range_slider.js_link('value', p1.x_range, 'end', attr_selector=1)
-    range_slider.js_link('value', box, 'left', attr_selector=0)
-    range_slider.js_link('value', box, 'right', attr_selector=1)
+    range_slider.js_link('value', p1.x_range, 'start', attr_selector = 0)
+    range_slider.js_link('value', p1.x_range, 'end', attr_selector = 1)
+    range_slider.js_link('value', p3.x_range, 'start', attr_selector = 0)
+    range_slider.js_link('value', p3.x_range, 'end', attr_selector = 1)
+    range_slider.js_link('value', box, 'left', attr_selector = 0)
+    range_slider.js_link('value', box, 'right', attr_selector = 1)
     
-    callback = CustomJS(
+    p1Callback = CustomJS(
         args = dict(
             xr = p1.x_range,
             yr = p1.y_range,
@@ -263,14 +264,41 @@ def index(request, code):
                 yr.start = y_start;
                 yr.end = y_end;
             """
-        )
-    p1.x_range.js_on_change('start', callback)
+    )
+    p1.x_range.js_on_change('start', p1Callback)
+
+    crosshair = CrosshairTool(dimensions = "both")
+    p1.add_tools(crosshair)
+    p3.add_tools(crosshair)
+   
+    p3Callback = CustomJS(
+        args = dict(
+            xr = p3.x_range,
+            yr = p3.y_range,
+            data = source.data
+        ),
+        code =
+            """
+                let begin = Math.floor(xr.start);
+                let end = Math.floor(xr.end);
+                let array = data.volume.slice(begin, end);
+                
+                let y_start = Math.min(...array) * 0.95;
+                let y_end = Math.max(...array) * 1.05;
+
+                yr.start = y_start;
+                yr.end = y_end;
+            """
+    )
+    p3.x_range.js_on_change('start', p3Callback)
     
     stock_layout = layout(
         [
             [p1],
+            [p3],
             [p2],
-            row(Spacer(width=20),range_slider)
+            
+            row(range_slider)
         ]
     )
     
